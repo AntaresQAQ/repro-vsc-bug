8ab09977b9277d450149aedca085bcdb07aaf049
Antares <antares.mi@outlook.com>
2025-11-21 22:01:58 +0800

    repro

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default tseslint.config(
+    { ignores: ["node_modules/**", "dist/**", ".yarn/*", ".pnp.*", "eslint.config.mjs"] },
+    tseslint.configs.recommendedTypeChecked,
+    prettierRecommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+            parserOptions: {
+                project: "tsconfig.json",
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "error",
+            "prettier/prettier": [
+                "error",
+                {},
+                {
+                    usePrettierrc: true,
+                },
+            ],
+        },
+    },
+);
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+const a = 1;
+const b = 2;
+
+function main() {
+    console.log("Hello, world!", a);
+}
+
+main();
