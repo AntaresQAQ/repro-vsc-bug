65a456aa2b215257274fc9a0a542274dbe98e960
Antares <antares.mi@outlook.com>
2025-11-21 22:09:02 +0800

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import prettierRecommended from "eslint-plugin-prettier/recommended";
+import { defineConfig } from 'eslint/config';
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
 
-
-export default tseslint.config(
+export default defineConfig(
     { ignores: ["node_modules/**", "dist/**", ".yarn/*", ".pnp.*", "eslint.config.mjs"] },
     tseslint.configs.recommendedTypeChecked,
     prettierRecommended,
